0f98a9df1c12069a4cc6ec48ff22a2931bc55e99
bbwieland <benbowenwieland@gmail.com>
2022-07-02 11:25:36 -0500

    uploading men's and women's game data

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Game-level data for the college men's 2021-22 season.
+#'
+#' A dataset containing information on each of the 6,752 documented men's ultimate frisbee games played at the collegiate level in the 2021-22 season.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{https://www.frisbee-rankings.com/}
+"gamesM"
+
+#' Game-level data for the college women's 2021-22 season.
+#'
+#'A dataset containing information on each of the 4,220 documented women's ultimate frisbee games played at the collegiate level in the 2021-22 season.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{https://www.frisbee-rankings.com/}
+"gamesW"
